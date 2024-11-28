eb6ed5b288f0087249e172ebd5643468925da5e0
Exile-Mu <1147799328@qq.com>
2024-11-29 00:14:19 +0800

    升级版本

--- src/main/java/com/exile/sentineldemo/exception/SentinelBlockException.java
+++ src/main/java/com/exile/sentineldemo/exception/SentinelBlockException.java
@@ -1,19 +1,20 @@
 package com.exile.sentineldemo.exception;
 
-import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc_v6x.callback.BlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Component
 public class SentinelBlockException implements BlockExceptionHandler {
+
     @Override
-    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse response, BlockException e) throws Exception {
+    public void handle(HttpServletRequest request, HttpServletResponse response, String s, BlockException e) throws Exception {
         log.warn("Blocked by Sentinel, block by function {{handle}}: {}", e.getRule());
         response.setContentType("application/json");
         response.setStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
